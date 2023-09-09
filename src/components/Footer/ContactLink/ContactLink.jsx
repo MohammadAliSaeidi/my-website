eada36214948f66d1eddcfad3ca9c75b9407d405
MohammadAli Saeidi <1saeidima@gmail.com>
2023-09-09 20:09:55 +0330

    fix: add rel="noopener noreferrer" to ContactLink component to the a tag for security reasons

--- src/components/Footer/ContactLink/ContactLink.jsx
+++ src/components/Footer/ContactLink/ContactLink.jsx
@@ -2,7 +2,7 @@ import './ContactLink.css'
 
 export default function ContactLink({link, icon, label}) {
     return (
-        <a className={'contact-link'} href={link} target="_blank">
+        <a className={'contact-link'} href={link} target="_blank" rel="noopener noreferrer">
             <div className={'icon'}>
                 {icon}
             </div>
